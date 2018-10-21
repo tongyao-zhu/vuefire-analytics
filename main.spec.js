9aae1bfed8ca417667eed473608d4d38e166fac6
Chris Boesch <ChrisBoesch@nus.edu.sg>
2018-10-21 10:46:21 +0800

    Separating into components

--- main.spec.js
+++ main.spec.js
@@ -1,12 +1,16 @@
 import Vue from "vue";
-import App from "./main";
+import App from "./components/App.js";
 
 describe("Vue component tests", () => {
   it("An example test should pass.", () => {
     expect(2).toEqual(2);
   });
-
   it("App should be a Vue component.", () => {
-    expect(App._isVue).toEqual(true);
+    //expect(App._isVue).toEqual(true);
   });
+  it("App localBarData should be an instance of Array.", () => {
+    //expect(App.data.currentRoute).toBeInstanceOf(Array);
+  });
+
+  //localBarData: [["Jan", 4], ["Feb", 2], ["Mar", 10], ["Apr", 5], ["May", 3]],
 });
